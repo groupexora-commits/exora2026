a4789d65dca98d3fa2a37230ecae01222cca7359
groupexora <groupexora@gmail.com>
2026-08-10 22:58:34 -0400

    Fix file upload fetch payload for Netlify

--- js/script.js
+++ js/script.js
@@ -151,8 +151,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetch("/", {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString()
+      body: formData
     })
       .then((response) => {
         if (!response.ok) {
